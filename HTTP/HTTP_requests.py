fc6b2dce8228b269bb281d241d2083c411cf647c
Diego Sanchez <diegosanchez.2340@gmail.com>
2023-10-24 12:27:30 -0500

    different Paths and separate folders

---
+++ HTTP/HTTP_requests.py
@@ -0,0 +1,67 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import requests
+
+class requestsHTTP(BaseHTTPRequestHandler):
+    #GET request HTTP
+    def do_GET(self):
+        if self.path == "/imageStreaming":
+            # Behandle Anfragen an den Pfad "/path1"
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Dies ist Pfad 1.")
+
+        elif self.path == "/":
+            # Behandle Anfragen an den Pfad "/path2"
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            # open and read the HTML file
+            with open("index.html", "r") as file:
+                html_content = file.read()
+
+            self.wfile.write(bytes(html_content, "utf-8"))
+
+        else:
+            # Standardverhalten für unbekannte Pfade
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Pfad nicht gefunden.")
+
+    #POST request HTTP
+    def do_POST(self):
+        global urlFlash
+        if self.path == "/flash":  #endpoint for the path
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            post_data = json.loads(post_data)
+
+            #Data received in post_data
+            print("Datos POST recibidos:", post_data)
+            if post_data.get("mensaje") == "flashON":
+                response = requests.get(urlFlash)
+
+            self.send_response(200)
+            self.end_headers()
+            #self.wfile.write(b"Solicitud POST recibida exitosamente")
+        else:
+            print("PATH: " + self.path + " not found")
+
+class RunServer:
+    def __init__(self, host, port, flash):
+        self.HOST = host
+        self.PORT = port
+        global urlFlash
+        urlFlash = flash
+
+    def run(self):
+        self.server = HTTPServer((self.HOST, self.PORT), requestsHTTP)
+        print("Server running...")
+        self.server.serve_forever()
+
+    def close(self):
+        self.server.server_close()
+        print("Server closed")
--- Test_Webserver.py
+++ Test_Webserver.py
@@ -1,13 +1,11 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
+from HTTP.HTTP_requests import RunServer
 import cv2
 import requests
 
-url = "http://192.168.137.211/1024x768.jpg"
-urlFlash="http://192.168.137.211/800x600.jpg"
+url = "http://192.168.137.103/640x480.jpg"
+urlFlash="http://192.168.137.103/800x600.jpg"
 
-
-HOST = "192.168.1.114"
+HOST = "10.203.132.106"
 #HOST = "localhost"
 PORT = 8000
 
@@ -22,41 +20,6 @@ try:
 except requests.exceptions.RequestException as e:
     print(f"Failed to access the URL. Error: {e}")
 
-class requestsHTTP(BaseHTTPRequestHandler):
-    #GET request HTTP
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        # open and read the HTML file
-        with open("index.html", "r") as file:
-            html_content = file.read()
-
-        self.wfile.write(bytes(html_content, "utf-8"))
-
-    #POST request HTTP
-    def do_POST(self):
-        if self.path == "/":  #endpoint for the path
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length).decode('utf-8')
-            post_data = json.loads(post_data)
-
-            #Data received in post_data
-            print("Datos POST recibidos:", post_data)
-            if post_data.get("mensaje") == "flashON":
-                response = requests.get(urlFlash)
-
-            self.send_response(200)
-            self.end_headers()
-            #self.wfile.write(b"Solicitud POST recibida exitosamente")
-        else:
-            print("PATH: " + self.path + " not found")
-
-
-server = HTTPServer((HOST, PORT), requestsHTTP)
-print("Server running...")
-
-server.serve_forever()
-server.server_close()
-print("Served closed")
+server = RunServer(HOST, PORT, urlFlash)
+server.run()
+server.close()
